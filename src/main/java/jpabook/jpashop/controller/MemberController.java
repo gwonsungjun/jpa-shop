e6288d49d4c6b5e853823de245f4e4596b2789be
gwonsungjun <sungjunpizz@gmail.com>
2021-01-06 17:00:21 +0900

    feat: 조회용 샘플 데이터 입력

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -45,6 +45,6 @@ public class MemberController {
     public String list(Model model) {
         List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
-        return "members/3";
+        return "members/memberList";
     }
 }
